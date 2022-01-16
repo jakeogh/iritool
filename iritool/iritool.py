2c8b2a56811135df8ded8e85033c815540373b70
jakeogh <github.com@v6y.net>
2022-01-15 22:05:53 -0700

    auto-commit

--- iritool/iritool.py
+++ iritool/iritool.py
@@ -204,6 +204,7 @@ class UrlparseResult(IriBase):
         self.username = self.urlparse.username
         self.domain = self.urlparse.netloc
 
+        ic(self.scheme)
         if self.verbose:
             ic(self.urlparse, self.iri, self.link_text, self.fragment, self.geturl, self.hostname, self.netloc, self.params, self.password, self.path, self.scheme, self.query, self.username, self.domain, self.domain_tld, self.domain_psl, self.domain_sld)
         assert self.scheme is not None
